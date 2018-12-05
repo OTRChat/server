3b3e139b3c673079a16ae86789990bcd35c0ea22
Dilan Guneratne <dilanguneratne@gmail.com>
2018-12-05 18:43:11 -0500

    added es6

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var shortid = require('shortid');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-var port = process.env.PORT || 9876;
+const port = process.env.PORT || 9876;
 
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
   // When a new user connects then we output a log
-  console.log('A user has connected: ' + socket.id);
+  console.log(`A user has connected: ${socket.id}`);
 
   // When the server receives a new message to send we broadcast it to the other users on the server
-  socket.on('new message', function(message) {
+  socket.on('new message', (message) => {
     // Broadcast will send to all users except the client that sent the message
     socket.broadcast.emit('new message', {
       username: socket.username,
-      message: message,
+      message,
       avatar: socket.avatar,
       messageClass: 'from-them blackText',
     });
   });
 
   // When a user disconnects then log that to the server
-  socket.on('disconnect', function() {
-    console.log('User: ' + socket.username + ' has disconnected.' + socket.id);
+  socket.on('disconnect', () => {
+    console.log(`User: ${socket.username} has disconnected. ${socket.id}`);
     socket.broadcast.emit('user disconnected', { username: socket.username });
   });
 
-  socket.on('typing', function() {
+  socket.on('typing', () => {
     socket.broadcast.emit('typing', {
       username: socket.username,
     });
   });
 
-  socket.on('stop typing', function() {
+  socket.on('stop typing', () => {
     socket.broadcast.emit('stop typing', {
       username: socket.username,
     });
   });
 
   // When a new user is registered (they make a username), set their username on the server.
-  socket.on('add user', function(username) {
+  socket.on('add user', (username) => {
     socket.username = username;
   });
 
   // When a user adds a custom avatar it sets it on the server.
-  socket.on('add avatar', function(avatar) {
+  socket.on('add avatar', (avatar) => {
     socket.avatar = avatar;
-    console.log('add new avatar' + socket.avatar);
+    console.log(`add new avatar ${socket.avatar}`);
   });
 
   // When a new user joins broadcast it to the other users on the server
   // Also set the servers username.
-  socket.on('user join', function(username) {
+  socket.on('user join', (username) => {
     socket.username = username;
     if (socket.username) {
       console.log("on user joined will broadcast that user '" + socket.username + "' joined.");
@@ -61,6 +60,6 @@ io.on('connection', function(socket) {
 });
 
 // Create the server - log the port number we are using
-server.listen(port, function() {
-  console.log('listening on *:' + port);
+server.listen(port, () => {
+  console.log(`listening on *: ${port}`);
 });
